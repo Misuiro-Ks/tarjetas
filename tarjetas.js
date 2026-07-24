8613dccd4e40437c92629642933ad4b44702f553
unknown <fabian.ni.fr@gmail.com>
2026-07-24 13:06:27 -0500

    pintar tarjetas segun rango desde-hasta

--- tarjetas.js
+++ tarjetas.js
@@ -1,8 +1,12 @@
 function crearTarjetas() {
     let contenido = "";
     let divTarjetas = document.getElementById("divTarjetas");
-    for (let i = 1; i <= 5; i++) {
-        contenido = contenido + "<div class= 'item'>" + i + "</div>";
+
+    let desde = Number(document.getElementById("txtDesde").value);
+    let hasta = Number(document.getElementById("txtHasta").value);
+
+    for (let i = desde; i <= hasta; i++) {
+        contenido = contenido + "<div class='item'>" + i + "</div>";
         divTarjetas.innerHTML = contenido;
     }
 }
